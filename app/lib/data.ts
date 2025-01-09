2d89e26b5bfb5f8f4da0aaca00814cd127129858
Jan Dranreb Balangue <jandranrebbalangue@Jan.local>
2025-01-09 15:04:07 +0800

    Remove setTimeout

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,12 +14,9 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -30,8 +27,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    console.log('Fetching  latest invoices data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -44,7 +39,6 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
-    console.log('Data fetch completed after 3 seconds.');
 
     return latestInvoices;
   } catch (error) {
